e9ce49c4e6d6da5bff3e49eaae1c500786e2f180
IbrahimSelim42 <ia2301024@tkh.edu.eg>
2025-10-19 19:02:02 +0300

    Delete EmbededSystemSubmission directory

--- EmbededSystemSubmission/Code/IO.h
+++
@@ -1,7 +0,0 @@
-#ifndef __IO__
-#define __IO__
-//Header file designed to mention the function and then be recalled throghout other files.
- void PullUp(void); //Initializing pin states.
- void SetBit(void); //Setting inputs and outputs.
- void PinInitial(void); //Enabling internal pull up resistor.
-#endif
